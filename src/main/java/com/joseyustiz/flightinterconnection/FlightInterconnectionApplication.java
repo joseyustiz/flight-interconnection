205605e308fc2fb683e2e18a309ae1e4bc3f6c9d
Jose Yustiz <jangyyustiz@falabella.cl>
2021-10-23 10:36:06 -0300

    initial project

---
+++ src/main/java/com/joseyustiz/flightinterconnection/FlightInterconnectionApplication.java
@@ -0,0 +1,13 @@
+package com.joseyustiz.flightinterconnection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightInterconnectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlightInterconnectionApplication.class, args);
+    }
+
+}
