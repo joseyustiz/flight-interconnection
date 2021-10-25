fa916e4e85a72f19e219251e6265354f7854a247
Jose Yustiz <jose.yustiz@gmail.com>
2021-10-25 03:32:16 -0300

    added @Configuration missing at ApplicationConfig

--- src/main/java/com/joseyustiz/flightinterconnection/infrastructure/config/ApplicationConfig.java
+++ src/main/java/com/joseyustiz/flightinterconnection/infrastructure/config/ApplicationConfig.java
@@ -3,7 +3,9 @@ package com.joseyustiz.flightinterconnection.infrastructure.config;
 import com.joseyustiz.flightinterconnection.core.GetInterconnectedFlightUseCase;
 import com.joseyustiz.flightinterconnection.core.service.GetInterconnectedFlightService;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class ApplicationConfig {
     @Bean
     public GetInterconnectedFlightUseCase getInterconnectedFlightUseCase(){
