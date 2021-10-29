9a4992f84525dd240139e0a80616b4f501ad4713
Jose Yustiz <jose.yustiz@gmail.com>
2021-10-28 22:29:25 -0300

    created bean for InterconnectedFlightMapper

--- src/main/java/com/joseyustiz/flightinterconnection/infrastructure/config/ApplicationConfig.java
+++ src/main/java/com/joseyustiz/flightinterconnection/infrastructure/config/ApplicationConfig.java
@@ -2,6 +2,7 @@ package com.joseyustiz.flightinterconnection.infrastructure.config;
 
 import com.joseyustiz.flightinterconnection.core.GetInterconnectedFlightUseCase;
 import com.joseyustiz.flightinterconnection.core.service.GetInterconnectedFlightService;
+import com.joseyustiz.flightinterconnection.infrastructure.primary.web.InterconnectedFlightMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,4 +12,10 @@ public class ApplicationConfig {
     public GetInterconnectedFlightUseCase getInterconnectedFlightUseCase(){
         return new GetInterconnectedFlightService();
     }
+
+    @Bean
+    public InterconnectedFlightMapper getInterconnectedFlightMapper(){
+        return InterconnectedFlightMapper.INSTANCE;
+    }
+
 }
